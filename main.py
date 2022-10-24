9c80906b5aadcc9bd8860b81e25fe6ae8fc19dbe
Gimmesamoa <silversix311@gmail.com>
2022-10-24 18:19:32 -0400

    Added error checking on message_id within !reload and on start

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import emoji
 import json
 import DiscordUtils
+import re
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -29,8 +30,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global roleMessage
     ## If message.id == "BOT_CHANNEL_ID"
     if message.channel.id == int(BOT_CHANNEL_ID):
+    ## if message is from bot, return
+        if message.author == client.user:
+            return
         if message.content == "!restart":
             await message.channel.send("Restarting...")
             python = sys.executable
@@ -42,6 +47,112 @@ async def on_message(message):
             roles = [role.name for role in roles]
             roles = "\n".join(roles)
             await message.channel.send(f"Your roles are:\n{roles}")
+            return
+        ## if message.content contains "!addrole" then set new_role variable to the 2nd word in the message and set new_description variable to the 3rd word in the message and set new_react variable to the 4th word in the message
+        if "!addrole" in message.content:
+            if message.content.split()[1] == "help":
+                await message.channel.send("Usage: !addrole Test_Role Test_Description <:gimmetail:872298447393419294>")
+                return
+            ## if 4th word in message.content exists send message "Usage: !addrole Test_Role Test_Description <:gimmetail:872298447393419294>"
+            if len(message.content.split()) > 4:
+                await message.channel.send("You entered incorrect arguments. Usage: !addrole Test_Role Test_Description <:gimmetail:872298447393419294>")
+                return
+            new_role = message.content.split()[1]
+            new_description = message.content.split()[2]
+            new_react = message.content.split()[3]
+            new_react_emoji = re.sub(r'[0-9<>]', '', new_react)
+
+            roles = message.guild.roles
+            if new_role in roles:
+                await message.channel.send("Role already exists.")
+            else:
+                # Get the ID of the emoji from the server and set it to new_react_id
+                for custom_emoji in client.emojis:
+                    if custom_emoji.name in new_react:
+                        new_react_id = custom_emoji.id
+                        # Add the new role to the config file
+                        with open(configFileLocation, "r") as configFile:
+                            configData = json.load(configFile)
+                            configuredRoles = configData["roles"]
+                        configuredRoles.append(
+                            {
+                                "react": new_react_emoji,
+                                "react_id": new_react_id,
+                                "role": new_role,
+                                "description": new_description,
+                                "role_id": 0
+                            }
+                        )
+                configData["roles"] = configuredRoles
+                with open(configFileLocation, "w") as configFile:
+                    json.dump(configData, configFile, indent=4)
+                # Send a message to the channel to confirm the role has been added
+                await message.channel.send(f"Added role {new_role} to the config file.")
+                return
+                
+        ## Remove a role from the config file
+        if "!delrole" in message.content:
+            if message.author == client.user:
+                return
+            ## if 2nd word in message.content exists send message "Usage: !delrole Test_Role"
+            if len(message.content.split()) > 2:
+                await message.channel.send("You entered incorrect arguments. Usage: !delrole Test_Role")
+                return
+            role_to_remove = message.content.split()[1]
+            ## check if role_to_remove is in the config file
+            with open(configFileLocation, "r") as configFile:
+                configData = json.load(configFile)
+                configuredRoles = configData["roles"]
+            for role in configuredRoles:
+                if role_to_remove == role["role"]:
+                    configuredRoles.remove(role)
+                    configData["roles"] = configuredRoles
+                    with open(configFileLocation, "w") as configFile:
+                        json.dump(configData, configFile, indent=4)
+                    await message.channel.send(f"Removed role {role_to_remove} from the config file.")
+                    return
+
+
+        ## List all roles in the config file
+        if "!listroles" in message.content:
+            with open(configFileLocation, "r") as configFile:
+                configData = json.load(configFile)
+                configuredRoles = configData["roles"]
+            configuredRoles = [role["role"] for role in configuredRoles]
+            configuredRoles = "\n".join(configuredRoles)
+            await message.channel.send(f"Configured roles are:\n{configuredRoles}")
+            return
+
+        ## Reload reaction message
+        if "!reload" in message.content:
+            if message.author == client.user:
+                return
+            await message.channel.send("Reloading reaction message...")
+            channel = await client.fetch_channel(CHANNEL_ID)
+            roles = await client.guilds[0].fetch_roles()
+            map_role_ID(roles)
+            map_emoji_ids()
+
+            if role_message_exists():
+                roleMessage = get_message_id()
+                if roleMessage != 0:
+                    try :
+                        roleMessage = await channel.fetch_message(roleMessage)
+                    except discord.errors.NotFound:
+                        roleMessage = await channel.send(content=build_message())
+                        store_message_id(roleMessage.id)
+                await roleMessage.edit(content=build_message())
+            else:
+                roleMessage = await channel.send(content=build_message())
+                store_message_id(roleMessage.id)
+            # Remove all reactions from the bot on the message.
+            for react in roleMessage.reactions:
+                await react.remove(client.user)
+        
+            # Add a reaction from the bot for each in the config
+            default_reacts = get_all_reacts()
+            for react in default_reacts:
+                await roleMessage.add_reaction(emoji.emojize(react, language='alias'))
 
 def interpret_emoji(payload):
     """
@@ -238,7 +349,13 @@ async def on_ready():
     map_emoji_ids()
 
     if role_message_exists():
-        roleMessage = await channel.fetch_message(get_message_id())
+        roleMessage = get_message_id()
+        if roleMessage != 0:
+            try :
+                roleMessage = await channel.fetch_message(roleMessage)
+            except discord.errors.NotFound:
+                roleMessage = await channel.send(content=build_message())
+                store_message_id(roleMessage.id)
         await roleMessage.edit(content=build_message())
     else:
         roleMessage = await channel.send(content=build_message())
@@ -253,12 +370,6 @@ async def on_ready():
     for react in default_reacts:
         await roleMessage.add_reaction(emoji.emojize(react, language='alias'))
 
-# @client.event
-# ## add user to role
-# async def on_member_join(member):
-#     # Get the role object
-#     role = discord.utils.get(member.guild.roles, name="Member")
-#     # Add the role to the user
-#     await member.add_roles(role)
+
 
 client.run(DISCORD_TOKEN)
